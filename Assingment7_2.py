9147443f964a3d98dfbd71f90ccda2e50faa8fc5
AndreaW123 <71686485+AndreaW123@users.noreply.github.com>
2020-09-27 13:01:54 -0400

    Add files via upload

---
+++ Assingment7_2.py
@@ -0,0 +1,18 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: /Users/thurmanwilson/Downloads/peerReviewErrors.py =========
+You are in a land full of dragons. In front of you,
+you see two caves. In one cave, the dragon is friendly
+and will share his treasure with you. The other dragon
+is greedy and hungry, and will eat you on sight.
+
+Which cave will you go into? (1 or 2)
+2
+You approach the cave...
+It is dark and spooky...
+A large dragon jumps out in front of you! He opens his jaws and...
+
+Gives you his treasure!
+Do you want to play again? (yes or no)
